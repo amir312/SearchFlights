0d5b7c7990ccfba40adc4fc6e08f2bdb2ec2d628
amir312 <57588920+amir312@users.noreply.github.com>
2019-11-20 12:29:39 +0200

    Add files via upload

---
+++ FlightsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HW1.Models;
+
+namespace HW1.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        // GET api/<controller>
+        public List<Flight> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            Flight flt1 = new Flight();
+            return flt1.getFlight();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
