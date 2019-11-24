01b09a7f45190d63b5d90b6fdc5a27b7d95194ae
amir312 <57588920+amir312@users.noreply.github.com>
2019-11-24 19:20:43 +0200

    Add files via upload

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -21,18 +21,19 @@ namespace HW1.Models
         public float Price { get => price; set => price = value; }
         public string FlyFrom { get => flyFrom; set => flyFrom = value; }
         public string FlyTo { get => flyTo; set => flyTo = value; }
-        public string DepartDate { get => DepartDate; set => DepartDate = value; }
-        public string ReturnDate { get => ReturnDate; set => ReturnDate = value; }
+        public string DepartDate { get => departDate; set => departDate = value; }
+        public string ReturnDate { get => returnDate; set => returnDate = value; }     
         public List<string> Subflights { get => subflights; set => subflights = value; }
         public List<string> Airlines { get => airlines; set => airlines = value; }
-       
+
+
         public Flight() { }
         public Flight(float price, string flyfrom, string flyto, string departdate, string returedate, List<string> subflight, List<string> airlines )
         {
             Price = price;
             FlyFrom = flyFrom;
             FlyTo = flyto;
-            DepartDate = departDate;
+            DepartDate = departdate;
             ReturnDate = returedate;
             Subflights = subflight;
             Airlines = airlines;
